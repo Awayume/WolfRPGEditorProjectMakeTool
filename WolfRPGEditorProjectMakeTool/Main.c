7d40c1b4812e4bf3e33c94c793f6d87511978ceb
粟夢 <79361022+Awayume@users.noreply.github.com>
2021-07-26 22:15:11 +0900

    Update Main.c

--- WolfRPGEditorProjectMakeTool/Main.c
+++ WolfRPGEditorProjectMakeTool/Main.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
 int main(vold) {
+  printf("=======================================\nWolfRPGエディタープロジェクト作成ツール\n=======================================\n");
   
+  FILE *file;
+ 
+  if (file = fopen("WolfRPGEditorProjectMakeTool.ini", "r"))
+    {
+      fclose(file);
+      printf("OK.\n");
+    }
+  else
+    {
+      printf("Not Found.\n");
+      return 1;
+    }
+  if (file =) setopne
+  return 0;
 }
